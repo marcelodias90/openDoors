4d62f8409485d625d9c6dc8fb53b12d275688c86
Marcelo Dias <diasteixeira1990@gmail.com>
2023-01-06 09:49:53 -0300

    adicionando porta, typscrypt na aplicação

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,13 @@
-import Presente from "../components/Presente";
+import { useState } from "react";
+import Porta from "../components/Porta";
+import PortaModel from "../model/porta";
+
 
 export default function Home() {
+  const [p1, setP1] = useState(new PortaModel(1))
   return (
-    <di>
-        <Presente/>
-    </di>
+    <div style={{display: "flex"}}>
+        <Porta porta={p1}/>
+    </div>
   )
 }
